18fdfebab20831fa3c705757db830bef38e7ec14
vinaya-53 <vinuu53@gmail.com>
2024-07-02 18:19:00 +0530

    2nd  commit

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
